d1630151b7dd1ad59d6e2d48d76c55e192d5a468
Elasticated-Shoe <31132612+Elasticated-Shoe@users.noreply.github.com>
2019-02-17 14:08:29 +0000

    Removed Unused Controller

--- protected/apiController.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template, request, session
-
-def init():
-    if request.method == "GET" and request.query_string == b'':
-        if session["settingsAccess"] == 1:
-            print(request.query_string)
-        else:
-            return "403"

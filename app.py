fb34bf1b176f245c9b4382a251b7e3617432d732
Elasticated-Shoe <31132612+Elasticated-Shoe@users.noreply.github.com>
2019-02-17 14:07:31 +0000

    Speed Improvements; Minifies Resources, Creates Vendor Bundle, Async Script Load

--- app.py
+++ app.py
@@ -1,32 +1,29 @@
 from flask import Flask
 import protected.abstractions.globals as shared
 import protected.abstractions.caching as cacheAb
-import protected.abstractions.fileOperations as fileOp
-import protected.dashboardController as dashController
-import protected.loginController as loginController
-import protected.noPageController as pageNotFoundController
 
 app = Flask(__name__, template_folder='protected/views') # create flask app object, specify different folder for templates
+
 app.config['SECRET_KEY'] = shared.globalSettings["flask"]["key"] # read secret key from config file
 cacheAb.init(shared.cache, app) # initiate the cache
 
 @app.before_request # before every request, except ones to the static folder, check user is logged in
 def runPreCheck():
+    import protected.loginController as loginController
     if loginController.check() == False:
         return loginController.init()
 
 @app.route('/', methods=["POST", "GET"]) # return main page
 def index():
+    import protected.dashboardController as dashController
     return dashController.init()
-
-@app.route('/API', methods=["GET"]) # respond with data upon request
-def dataAPI():
-    #print("file object: " + str(fileOp.getFileList({"tv": [r"S:\GIT Repos\Media-Manager\testdir\tv"], "film": [r"S:\GIT Repos\Media-Manager\testdir\film"]})))
-    return "fuck"
 
 @app.errorhandler(404) # 404 error handler
 def pageNotFound(e):
+    import protected.noPageController as pageNotFoundController
     return pageNotFoundController.render404()
 
 if __name__ == '__main__':
+    import protected.abstractions.prepare as initial
+    initial.startUp()
     app.run(debug=False, use_reloader=False, host='0.0.0.0', port = 5006, threaded=True)
--- app.wsgi
+++ app.wsgi
@@ -1,3 +1,6 @@
 import os, sys
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 from app import app as application
+
+import protected.abstractions.prepare as initial
+initial.startUp()

c0a918f2ba433c935f877128aaa1be018a23c03c
Elasticated-Shoe <31132612+Elasticated-Shoe@users.noreply.github.com>
2019-02-17 14:08:19 +0000

    Fixed wsgi Error Due To File Not Found

--- protected/abstractions/globals.py
+++ protected/abstractions/globals.py
@@ -1,12 +1,14 @@
 from flask_caching import Cache
 import configparser
+import os
 
 # Cache Should only be created once, then all should use this same object
 cache = Cache(config={'CACHE_TYPE': 'simple'})
 
 # Create config object
 config = configparser.ConfigParser() # Initialise the config parser
-config.read("protected/config.ini") # Read the config file
+configFile = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'config.ini'))
+config.read(configFile) # Read the config file
 globalSettings = {}
 for section in config: # map from file to object
     globalSettings[section] = {}
